11aaad7adf7395d4bd92a8b85501fa6523c73e7d
weslcurt <18757779+weslcurt@users.noreply.github.com>
2018-05-30 12:06:37 -0500

    Update app.js

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
   }
 }
 //2.0 stop
-/*NEW DB HEAD
+/*NEW DB HEAD sdfmsdv
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
